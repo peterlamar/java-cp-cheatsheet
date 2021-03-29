4af44d44089196e5da25cda59be9d926e5df59d6
plamar <plamar@hubspot.com>
2021-03-29 09:54:53 -0400

    practicing loops

--- leet/string/defangIPaddr.java
+++ leet/string/defangIPaddr.java
@@ -12,3 +12,16 @@ class Solution {
         return str.toString();
     }
 }
+
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()){
+            if (c == '.') sb.append("[.]");
+            else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
