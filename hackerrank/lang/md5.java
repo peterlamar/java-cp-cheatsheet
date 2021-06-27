9a534eb67bb91fda7776ba5c2a33ca5a2d74a804
plamar <plamar@hubspot.com>
2021-06-27 11:05:24 -0400

    md5 encryption

---
+++ hackerrank/lang/md5.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] digest = md.digest();
+            for (byte b : digest) {
+                System.out.printf("%02x", b);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
