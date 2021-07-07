48b24a9729d0f8d18627381e96621f5660187d86
plamar <plamar@hubspot.com>
2021-07-07 13:31:44 -0400

    Cake interview example

---
+++ cake/stack/getClosingParen.java
@@ -0,0 +1,60 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static int getClosingParen(String s, int intro) {
+        int cnt=0;
+            
+        for (int i=intro+1; i<s.length(); i++){
+            if (s.charAt(i) == '('){
+                cnt++;
+            }
+            else if (s.charAt(i) == ')'){
+                if (cnt == 0) {
+                    return i;
+                } else {
+                    cnt--;                
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No closing parenthesis :(");
+    }
+
+
+    // tests
+
+    @Test
+    public void allOpenersThenClosersTest() {
+        final int expected = 7;
+        final int actual = getClosingParen("((((()))))", 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mixedOpenersAndClosersTest() {
+        final int expected = 10;
+        final int actual = getClosingParen("()()((()()))", 5);
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void noMatchingCloserTest() {
+        getClosingParen("()(()", 2);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
