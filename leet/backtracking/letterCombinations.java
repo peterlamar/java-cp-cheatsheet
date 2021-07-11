7491e8f9803a85b72bc2aa608460f94e15c1fd4c
plamar <plamar@hubspot.com>
2021-07-11 19:15:58 -0400

    Backtracking!!

--- leet/backtracking/letterCombinations.java
+++ leet/backtracking/letterCombinations.java
@@ -16,7 +16,6 @@ class Solution {
 
         rtn.add("");
         for (int i=0; i<digits.length(); i++){
-            
             while (rtn.peek().length() == i){
                 String t = rtn.remove();
                 String tmpDigits = map.get(String.valueOf(digits.charAt(i)));
@@ -27,5 +26,29 @@ class Solution {
         }
                 
         return rtn;
+    }
+}
+
+
+class Solution {
+    public static String[] dict = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length()==0) return new ArrayList<>();
+        List<String> rtn = new ArrayList<>();
+        backtracking(rtn, digits.toCharArray(), "");
+        return rtn;
+    }
+    
+    public void backtracking(List<String> combos, char[] digits, String s){
+        if (s.length() == digits.length) {
+            combos.add(s);
+            return;
+        }
+        int i = s.length();
+        int digit = digits[i] - '0';
+        for (char c : dict[digit].toCharArray()){
+            backtracking(combos, digits, s + Character.toString(c));
+        }
     }
 }
